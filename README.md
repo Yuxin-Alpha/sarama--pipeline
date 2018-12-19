ba4b357dafd8c71e0500882150a5a32fae3aa48f
Clement_Alpha <956163372@qq.com>
2018-12-19 13:58:53 +0800

    bug 修改

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # cardList-App
-Just a small demo for Express
+This is just a small demo for Express with template EJS.
+
+
+
+To run up it, you need to install some npm packages
+
+```shell
+npm install
+```
+
+and then
+
+```shell
+npm run server
+```
+
+
+
+This APP is running on port 5000
--- controller/todoController.js
+++ controller/todoController.js
@@ -1,6 +1,15 @@
+let data = [
+  {item: "白天吃饭"},
+  {item: "晚上吃饭"},
+  {item: "白天睡觉"},
+  {item: "晚上睡觉"},
+  {item: "白天敲代码"},
+  {item: "晚上敲代码"},
+  {item: "美滋滋"}
+];
 module.exports = function (app) {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos: '123'});
   });
 
   app.post('/todo', (req, res) => {
