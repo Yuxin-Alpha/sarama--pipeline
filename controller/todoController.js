95fb216af6a223e5ba3b8fab44e8f644f6685b26
Clement_Alpha <956163372@qq.com>
2018-12-21 11:30:53 +0800

    添加与删除功能，链接数据库

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,18 +1,38 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://todo:todo123@ds141294.mlab.com:41294/todo')
+//创建图表
+let todoSchema = new mongoose.Schema({
+  item: String
+});
+let Todo = mongoose.model('Todo', todoSchema);
+
 let urlencodeParser = bodyParser.urlencoded({extended: false});
-let data = [
-  
-];
+
 module.exports = function (app) {
   app.get('/todo', (req, res) => {
-    res.render('todo', {todos: data});
+    Todo.find({}, (err, data) => {
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', urlencodeParser, (req, res) => {
-    data.push(req.body);
+    Todo(req.body).save((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    })
   });
 
-  app.delete('/todo', (req, res) => {
-    //coding
+  app.delete('/todo/:item', (req, res) => {
+    Todo.find({item: req.params.item}).remove((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
+    // data = data.filter((todo) => {
+    //   return req.params.item !== todo.item;
+    // });
+    // res.json(data);
   });
 }
