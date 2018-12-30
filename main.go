1ea4b7a82e79c0abc45ff2e1e35a619aafcf88ec
Yuxin <956163372@qq.com>
2018-12-30 23:35:24 +0800

    笔记更新

--- main.go
+++ main.go
@@ -3,7 +3,28 @@ import(
 	"fmt"
 )
 func main() {
-	var num1 int = 99
-	var str string = fmt.Sprintf("%d", num1)
-	fmt.Printf(str)
+	// 指针修改值
+	// var num int = 20
+	// var ptr *int = &num
+	// fmt.Println("num=", &num)
+	// *ptr = 30
+	// fmt.Println("num=", num)
+	
+	// 交换两个变量
+	// var a int = 10
+	// var b int = 50
+	// a = a + b
+	// b = a - b
+	// a = a - b
+	// var age int8 
+	// fmt.Println("请输入年龄")
+	// fmt.Scanln(&age)
+	// if age > 10 {
+	// 	fmt.Println("666")
+	// } else {
+	// 	fmt.Println("777")
+	// }
+	for i := 1; i <= 10; i++ {
+		fmt.Println("777")
+	}
 }
