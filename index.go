19d526d9d0f7c0613d72fd4a34d7524da67efcea
Yuxin <9561633712@qq.com>
2019-12-09 14:47:17 +0800

    [delete]删除原始代码

--- index.go
+++ index.go
@@ -1,21 +1,6 @@
 package main
 import "fmt"
 
-var (
-	str = "packageValue"
-)
-func variableZeroValue() {
-	var a int = 1
-	var s string = "123"
-	fmt.Println(a, s)
-}
-func variableShoter() {
-	a := 1
-	b := 2
-	fmt.Println(a+b)
-}
 func main() {
-	variableZeroValue()
-	variableShoter()
-	fmt.Println(str)
+	
 }
